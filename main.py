97e43c2169c9cacec865bd6da1047645a87c262a
DanielRendon1020 <117864127+DanielRendon1020@users.noreply.github.com>
2023-08-01 04:04:47 +0000

    answers

--- main.py
+++ main.py
@@ -6,52 +6,73 @@
 
 # What advantages are available when we're working with Python? If this Array was instead a set, we wouldn't be able to store multiple values in it. Uncomment the identical set below and run the print statement. What did you notice in the console return?
 
-# my_set = {12, 55, 33, 40, 55, 33, 20, 12}
+my_set = {12, 55, 33, 40, 55, 33, 20, 12}
 
-# print(my_set)
+print(my_set)
 
 #Question 1a: Create a set of your own with at least 3 different elements.
 
+set = {99 , 98, 97}
+
 #Question 1b: Add an item to the set that you just created.
 
+set.add(96)
+
 #Question 1c: Print the set with the new data that you added to it:
+
+print(set)
 
 
 #Question 2a: Create a tuple with at least 3 elements inside of it.
 
+tuple = (11, 22, 33)
+
 #Question 2b: Print the tuple you just created.
 
-
+print(tuple)
 
 
 #Section 2 - loops:
 
-# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 # Question 3: Use a for loop to iterate through the 'days' list above and print the days of the week:
 
+for d in days:
+  print(d)
 
-# x = 10
-# add_list = [10, 6, 12, 4, 5]
+x = 10
+add_list = [10, 6, 12, 4, 5]
 # Question 4: Uncomment the list and variable x above.  Loop through add_list and add each value to x. Print the value of x at each increment:
 
-
+for i in add_list:
+  print(i + x)
 
 
 # Question 5: While Loops 
 
 #Declare an iterator variable (set to the number 1) and write a While loop that adds 5 to the value of the variable starting_value as long as the iterator is under 10:
 
-#starting_value = 5
+starting_value = 5
+
+i = 1
+while i < 10:
+  starting_value += 5
+  print(starting_value)
+  i += 1
 
 
 #Section 3 - conditionals: if, elif, else statements
 
-# favorite_movie = "insert here"    
+favorite_movie = "Movie"    
 #Question 6a: Uncomment the favorite_movie variable above and change the value to the title of your favorite movie
 #Below, write a conditional statement that prints the string "that's a great movie" if the favorite_movie variable equals your favorite movie.
 #Otherwise (else), print the string "that's not my favorite movie".  
 #Mess around with the value of the favorite_movie variable and trigger both conditional responses:
 
+if favorite_movie == "Movie1":
+  print("that's a great movie")
+else:
+  print("Booooo!")
 
 
 #Question 6b: Uncomment the movie_list variable below and implement a for loop that iterates through each item in the list. 
@@ -59,7 +80,15 @@
 #if the item is a car manufacturing company like Toyota, print "item is a car and not a movie"; 
 #otherwise, just print the movie in the list:
 
-# movie_list = ["Inception", "blueberries", "Toyota", "Good Will Hunting"]
+movie_list = ["Inception", "blueberries", "Toyota", "Good Will Hunting"]
+
+for movie in movie_list:
+    if movie == "blueberries":
+        print("item is a fruit and not movie")
+    elif movie == "Toyota":
+        print("item is a car and not a movie")
+    else:
+        print(movie)
 
 
 
